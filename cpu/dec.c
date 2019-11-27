c4421dd4fedd486d04e90323846287f882421e69
Acquaviva Marco <acquavivamarco@hotmail.it>
2019-11-27 08:51:11 +0100

    Fix litte endian adressing, add dec tests for zero and negative flag, add inc tests for zero flag

--- cpu/dec.c
+++ cpu/dec.c
@@ -27,8 +27,8 @@ static int dec_zero_page_x(mos6502_t *cpu)
 
 static int dec_absolute(mos6502_t *cpu)
 {
-    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint8_t low = cpu->read(cpu, cpu->pc++);
+    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint16_t final_address = (high << 8) | low;
 
     cpu->a = mos6502_read16(cpu, final_address);
@@ -41,8 +41,8 @@ static int dec_absolute(mos6502_t *cpu)
 
 static int dec_absolute_x(mos6502_t *cpu)
 {
-    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint8_t low = cpu->read(cpu, cpu->pc++);
+    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint8_t increment = cpu->read(cpu, cpu->pc++);
     uint16_t address = (high << 8) | low;
     uint16_t final_address = address + (uint16_t)increment;
@@ -65,6 +65,7 @@ void mos6502_register_dec(mos6502_t *cpu)
 }
 
 #ifdef _TEST
+//REGULAR
 
 static int test_dec_zero_page(mos6502_t *cpu)
 {
@@ -89,8 +90,8 @@ static int test_dec_absolute(mos6502_t *cpu)
 {
     mos6502_write8(cpu, 0x1234, 0x55);
     mos6502_write8(cpu, 0x8000, 0xCE);
-    mos6502_write8(cpu, 0x8001, 0x12);
-    mos6502_write8(cpu, 0x8002, 0x34);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
     int ticks = mos6502_tick(cpu);
 
     return ticks = 6 && cpu->a == 0x54 && cpu->pc == 0x8003 && cpu->flags == 0;
@@ -100,19 +101,117 @@ static int test_dec_absolute_x(mos6502_t *cpu)
 {
     mos6502_write8(cpu, 0x1333, 0x55);
     mos6502_write8(cpu, 0x8000, 0xDE);
-    mos6502_write8(cpu, 0x8001, 0x12);
-    mos6502_write8(cpu, 0x8002, 0x34);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
     mos6502_write8(cpu, 0x8003, 0xff);
     int ticks = mos6502_tick(cpu);
 
     return ticks = 7 && cpu->a == 0x54 && cpu->pc == 0x8004 && cpu->flags == 0;
 }
 
+//ZERO
+static int test_dec_zero_page_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x0055, 0x01);
+    mos6502_write8(cpu, 0x8000, 0xC6);
+    mos6502_write8(cpu, 0x8001, 0x55);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 5 && cpu->a == 0x00 && cpu->pc == 0x8002 && cpu->flags == ZERO;
+}
+
+static int test_dec_zero_page_x_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x0056, 0x01);
+    mos6502_write8(cpu, 0x8000, 0xD6);
+    mos6502_write8(cpu, 0x8001, 0x55);
+    mos6502_write8(cpu, 0x8002, 0x01);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 6 && cpu->a == 0x00 && cpu->pc == 0x8003 && cpu->flags == ZERO;
+}
+
+static int test_dec_absolute_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x1234, 0x01);
+    mos6502_write8(cpu, 0x8000, 0xCE);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
+    int ticks = mos6502_tick(cpu);
+
+    return ticks = 6 && cpu->a == 0x00 && cpu->pc == 0x8003 && cpu->flags == ZERO;
+}
+
+static int test_dec_absolute_x_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x1333, 0x01);
+    mos6502_write8(cpu, 0x8000, 0xDE);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
+    mos6502_write8(cpu, 0x8003, 0xff);
+    int ticks = mos6502_tick(cpu);
+
+    return ticks = 7 && cpu->a == 0x00 && cpu->pc == 0x8004 && cpu->flags == ZERO;
+}
+
+//NEGATIVE
+static int test_dec_zero_page_negative_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x0055, 0x00);
+    mos6502_write8(cpu, 0x8000, 0xC6);
+    mos6502_write8(cpu, 0x8001, 0x55);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 5 && cpu->a == 0xFF && cpu->pc == 0x8002 && cpu->flags == NEGATIVE;
+}
+
+static int test_dec_zero_page_x_negative_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x0056, 0x00);
+    mos6502_write8(cpu, 0x8000, 0xD6);
+    mos6502_write8(cpu, 0x8001, 0x55);
+    mos6502_write8(cpu, 0x8002, 0x01);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 6 && cpu->a == 0xFF && cpu->pc == 0x8003 && cpu->flags == NEGATIVE;
+}
+
+static int test_dec_absolute_negative_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x1234, 0x00);
+    mos6502_write8(cpu, 0x8000, 0xCE);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
+    int ticks = mos6502_tick(cpu);
+
+    return ticks = 6 && cpu->a == 0xFF && cpu->pc == 0x8003 && cpu->flags == NEGATIVE;
+}
+
+static int test_dec_absolute_x_negative_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x1333, 0x00);
+    mos6502_write8(cpu, 0x8000, 0xDE);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
+    mos6502_write8(cpu, 0x8003, 0xff);
+    int ticks = mos6502_tick(cpu);
+
+    return ticks = 7 && cpu->a == 0xFF && cpu->pc == 0x8004 && cpu->flags == NEGATIVE;
+}
+
+// REGISTER TEST
 void test_mos6502_dec()
 {
     RUN_TEST(test_dec_zero_page);
     RUN_TEST(test_dec_zero_page_x);
     RUN_TEST(test_dec_absolute);
     RUN_TEST(test_dec_absolute_x);
+
+    RUN_TEST(test_dec_zero_page_zero_flag);
+    RUN_TEST(test_dec_zero_page_x_zero_flag);
+    RUN_TEST(test_dec_absolute_zero_flag);
+    RUN_TEST(test_dec_absolute_x_zero_flag);
+
+    RUN_TEST(test_dec_zero_page_negative_flag);
+    RUN_TEST(test_dec_zero_page_x_negative_flag);
+    RUN_TEST(test_dec_absolute_negative_flag);
+    RUN_TEST(test_dec_absolute_x_negative_flag);
 }
+
 #endif
--- cpu/inc.c
+++ cpu/inc.c
@@ -27,8 +27,8 @@ static int inc_zero_page_x(mos6502_t *cpu)
 
 static int inc_absolute(mos6502_t *cpu)
 {
-    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint8_t low = cpu->read(cpu, cpu->pc++);
+    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint16_t final_address = (high << 8) | low;
 
     cpu->a = mos6502_read16(cpu, final_address);
@@ -41,8 +41,8 @@ static int inc_absolute(mos6502_t *cpu)
 
 static int inc_absolute_x(mos6502_t *cpu)
 {
-    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint8_t low = cpu->read(cpu, cpu->pc++);
+    uint8_t high = cpu->read(cpu, cpu->pc++);
     uint8_t increment = cpu->read(cpu, cpu->pc++);
     uint16_t address = (high << 8) | low;
     uint16_t final_address = address + (uint16_t)increment;
@@ -65,6 +65,7 @@ void mos6502_register_inc(mos6502_t *cpu)
 }
 
 #ifdef _TEST
+//REGULAR
 
 static int test_inc_zero_page(mos6502_t *cpu)
 {
@@ -89,8 +90,8 @@ static int test_inc_absolute(mos6502_t *cpu)
 {
     mos6502_write8(cpu, 0x1234, 0x55);
     mos6502_write8(cpu, 0x8000, 0xEE);
-    mos6502_write8(cpu, 0x8001, 0x12);
-    mos6502_write8(cpu, 0x8002, 0x34);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
     int ticks = mos6502_tick(cpu);
 
     return ticks = 6 && cpu->a == 0x56 && cpu->pc == 0x8003 && cpu->flags == 0;
@@ -100,12 +101,56 @@ static int test_inc_absolute_x(mos6502_t *cpu)
 {
     mos6502_write8(cpu, 0x1333, 0x55);
     mos6502_write8(cpu, 0x8000, 0xFE);
-    mos6502_write8(cpu, 0x8001, 0x12);
-    mos6502_write8(cpu, 0x8002, 0x34);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
     mos6502_write8(cpu, 0x8003, 0xff);
     int ticks = mos6502_tick(cpu);
 
     return ticks = 7 && cpu->a == 0x56 && cpu->pc == 0x8004 && cpu->flags == 0;
+}
+
+//ZERO
+
+static int test_inc_zero_page_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x0055, 0xFF);
+    mos6502_write8(cpu, 0x8000, 0xE6);
+    mos6502_write8(cpu, 0x8001, 0x55);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 5 && cpu->a == 0x00 && cpu->pc == 0x8002 && cpu->flags == ZERO;
+}
+
+static int test_inc_zero_page_x_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x0056, 0xFF);
+    mos6502_write8(cpu, 0x8000, 0xF6);
+    mos6502_write8(cpu, 0x8001, 0x55);
+    mos6502_write8(cpu, 0x8002, 0x01);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 6 && cpu->a == 0x00 && cpu->pc == 0x8003 && cpu->flags == ZERO;
+}
+
+static int test_inc_absolute_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x1234, 0xFF);
+    mos6502_write8(cpu, 0x8000, 0xEE);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
+    int ticks = mos6502_tick(cpu);
+
+    return ticks = 6 && cpu->a == 0x00 && cpu->pc == 0x8003 && cpu->flags == ZERO;
+}
+
+static int test_inc_absolute_x_zero_flag(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x1333, 0xFF);
+    mos6502_write8(cpu, 0x8000, 0xFE);
+    mos6502_write8(cpu, 0x8001, 0x34);
+    mos6502_write8(cpu, 0x8002, 0x12);
+    mos6502_write8(cpu, 0x8003, 0xff);
+    int ticks = mos6502_tick(cpu);
+
+    return ticks = 7 && cpu->a == 0x00 && cpu->pc == 0x8004 && cpu->flags == ZERO;
 }
 
 void test_mos6502_inc()
@@ -114,5 +159,10 @@ void test_mos6502_inc()
     RUN_TEST(test_inc_zero_page_x);
     RUN_TEST(test_inc_absolute);
     RUN_TEST(test_inc_absolute_x);
+
+    RUN_TEST(test_inc_zero_page_zero_flag);
+    RUN_TEST(test_inc_zero_page_x_zero_flag);
+    RUN_TEST(test_inc_absolute_zero_flag);
+    RUN_TEST(test_inc_absolute_x_zero_flag);
 }
 #endif
